ccf8322b2011a2454e5faed1aaafdf71c3e6cd4a
mcraepv <61661042+mcraepv@users.noreply.github.com>
2020-07-20 20:29:47 -0500

    adds basic sidebar, sidenav

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SidenavTogglerComponent } from './sidenav-toggler/sidenav-toggler.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidenavTogglerComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
