33349f19ee299a40aa50308a88321ebcc39a5dfd
mcraepv <61661042+mcraepv@users.noreply.github.com>
2020-07-21 22:41:54 -0500

    adds server file

--- models/Cocktail.js
+++ models/Cocktail.js
@@ -1,29 +1,32 @@
 module.exports = function (sequelize, DataTypes) {
-  var Cocktail = sequelize.define('Cocktail', {
-    freezeTableName: true,
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
+  var Cocktail = sequelize.define(
+    'Cocktail',
+    {
+      freezeTableName: true,
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      instructions: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
     },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    instructions: {
-      type: DataTypes.TEXT,
-      allowNull: false
+    {
+      freezeTableName: true,
     }
-  }, {
-    freezeTableName: true,
-  });
+  );
 
   Cocktail.associate = function (models) {
-
     Cocktail.hasMany(models.CocktailIngredient, {
       onDelete: 'cascade',
       foreignKey: {
         allowNull: false,
-      }
+      },
     });
   };
 
---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var session = require('express-session');
+var passport = require('./config/passport');
+
+var PORT = process.env.PORT || 3000;
+var db = require('./models');
+
+// Creating express app and configuring middleware needed for authentication
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/api-routes.js')(app);
+
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log(
+      '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
+      PORT,
+      PORT
+    );
+  });
+});
