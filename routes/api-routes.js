3e25b6ddd86573bc0896d374dfc178f3a8d279b4
mcraepv <61661042+mcraepv@users.noreply.github.com>
2020-07-21 22:26:36 -0500

    adds authentication middleware, routes

---
+++ routes/api-routes.js
@@ -0,0 +1,26 @@
+const db = require('../models');
+const passport = require('../config/passport');
+
+module.exports = function (app) {
+  app.post('api/login', passport.authenticate('local'), (req, res) => {
+    res.json(req.user);
+  });
+
+  app.post('api/signup', (req, res) => {
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then(() => {
+        res.redirect(307, 'api/login');
+      })
+      .catch((err) => {
+        res.status(401).json(err);
+      });
+  });
+
+  app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+};
