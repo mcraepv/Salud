5663a20dd196baaa5678db7c4a8274940d822fba
mcraepv <61661042+mcraepv@users.noreply.github.com>
2020-07-23 22:24:46 -0500

    removes timestamps

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -1,24 +1,29 @@
 module.exports = function (sequelize, DataTypes) {
-  var Ingredient = sequelize.define('Ingredient', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
+  var Ingredient = sequelize.define(
+    'Ingredient',
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'Other',
+      },
     },
-    category: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: 'Other'
+    {
+      freezeTableName: true,
+      timestamps: false,
     }
-  }, {
-    freezeTableName: true
-  });
+  );
 
   Ingredient.associate = function (models) {
     Ingredient.hasMany(models.CocktailIngredient, {
       onDelete: 'cascade',
       foreignKey: {
-        allowNull: false
-      }
+        allowNull: false,
+      },
     });
   };
 
--- models/Measure.js
+++ models/Measure.js
@@ -1,19 +1,24 @@
 module.exports = function (sequelize, DataTypes) {
-  var Measure = sequelize.define('Measure', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
+  var Measure = sequelize.define(
+    'Measure',
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      freezeTableName: true,
+      timestamps: false,
     }
-  }, {
-    freezeTableName: true
-  });
+  );
 
   Measure.associate = function (models) {
     Measure.hasMany(models.CocktailIngredient, {
       onDelete: 'cascade',
       foreignKey: {
-        allowNull: false
-      }
+        allowNull: false,
+      },
     });
   };
 
