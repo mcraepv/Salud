e70cd4604a3982f2012af7740c3e318b2b2c39ab
mcraepv <61661042+mcraepv@users.noreply.github.com>
2020-07-27 18:24:43 -0500

    heroku fix?!

--- dist/client/polyfills.js
+++ dist/client/polyfills.js
@@ -3248,7 +3248,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/brandongatlin/GitHub/teaching/grading/03_2020/p2/Salud/client/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\mcrae\projects\Salud\client\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
