87d1a92b5da6bae61a556de5928d4992125bdee3
Tolu Alimi <tolualimi05@gmail.com>
2020-07-26 19:15:07 -0500

    Created user-favorites component

---
+++ client/src/app/user-favorites/user-favorites.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserFavoritesComponent } from './user-favorites.component';
+
+describe('UserFavoritesComponent', () => {
+  let component: UserFavoritesComponent;
+  let fixture: ComponentFixture<UserFavoritesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UserFavoritesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserFavoritesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
