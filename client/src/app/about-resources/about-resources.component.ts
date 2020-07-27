654a3a6c776b8d43011b48848618b4ea0bd8713f
Inez Escandon <iescan4@gmail.com>
2020-07-27 16:26:01 -0500

    finished about page [ci skip]

---
+++ client/src/app/about-resources/about-resources.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-resources',
+  templateUrl: './about-resources.component.html',
+  styleUrls: ['./about-resources.component.css']
+})
+export class AboutResourcesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
